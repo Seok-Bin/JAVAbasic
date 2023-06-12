cfdf9efda14802335ae2c901ceb32b01c4d5ce25
seokbin <dnltjrqls08@gamil.com>
2023-06-12 22:23:12 +0900

    javaSE project test 파일 커밋

---
+++ src/JavaSEProjectTest/FourArithmetic.java
@@ -0,0 +1,17 @@
+package JavaSEProjectTest;
+
+public class FourArithmetic {
+    public static void main(String[] args){
+        int a , b , sum, minus, multiply, division;
+        a = 12;
+        b = 2;
+        sum = a + b;
+        minus = a - b;
+        multiply = a * b;
+        division = a / b;
+        System.out.println("a+b= " + sum);
+        System.out.println("a-b= " + minus);
+        System.out.println("a*b= " + multiply);
+        System.out.println("a/b= " + division);
+    }
+}
---
+++ src/JavaSEProjectTest/HelloWorld.java
@@ -0,0 +1,7 @@
+package JavaSEProjectTest;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("안녕. World");
+    }
+}
