9cac4539188f91545ee0781ba3c3ec69f623f043
seokbin <dnltjrqls08@naver.com>
2023-06-16 23:34:25 +0900

    Uddt 패키지 생성

---
+++ src/UddtTest/Movie.java
@@ -0,0 +1,10 @@
+package UddtTest;
+
+public class Movie {
+    public String title;
+    public String openingDate;
+    public String protagonist;
+    public String movieGenre;
+    public int runningTime;
+    public String rated;
+}
