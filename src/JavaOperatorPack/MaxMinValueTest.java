aa708afb208882b4e8c827c125c39dfd16a2d085
seokbin <dnltjrqls08@naver.com>
2023-06-26 20:45:24 +0900

    OperatorPack 패키지 생성

---
+++ src/JavaOperatorPack/MaxMinValueTest.java
@@ -0,0 +1,16 @@
+package JavaOperatorPack;
+
+public class MaxMinValueTest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        //최소값
+        int min = (a < b) ? a : b;
+        System.out.println("min = " + min);
+
+        //최대값
+        int max = (a > b) ? a : b;
+        System.out.println("max = " + max);
+    }
+}
