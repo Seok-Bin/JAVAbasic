d0865c85aac0aa954dd07c89211a975fd03b6200
seokbin <dnltjrqls08@naver.com>
2023-06-28 01:03:24 +0900

    Conditional 패키지 생성

---
+++ src/ConditionalPack/IfElseIfTest.java
@@ -0,0 +1,28 @@
+package ConditionalPack;
+
+import java.util.Scanner;
+
+public class IfElseIfTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("성정 입력 : ");
+        int score = scan.nextInt();
+
+        if (score >= 0 && score <= 100){
+            if(score >= 80){
+                if (score >= 90){
+                    System.out.println("A");
+                } else if (score >= 85){
+                    System.out.println("B");
+                } else {
+                    System.out.println("C");
+                }
+            } else {
+                System.out.println("불합격");
+            }
+        } else {
+            System.out.println("유효하지 않은 점수");
+        }
+
+    }
+}
