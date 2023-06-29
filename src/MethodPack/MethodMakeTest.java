c9094c08eddb00dd8f493c8b691329c98987013e
seokbin <dnltjrqls08@naver.com>
2023-06-29 19:15:56 +0900

    MethodPack 패키지 생성

---
+++ src/MethodPack/MethodMakeTest.java
@@ -0,0 +1,24 @@
+package MethodPack;
+
+import java.util.Scanner;
+
+public class MethodMakeTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("정수 입력 : ");
+        int a = scan.nextInt();
+        System.out.print("정수 입력 : ");
+        int b = scan.nextInt();
+        // 만들어 놓은 add 메서드 호출
+
+        int result =  add(a, b);
+        System.out.println("총 합 = " + result);
+    }
+
+    // 매개변수로 2개의 정수를 받아 총합을 구해 리턴해주는 메서드 정의
+    public static int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+}
+
