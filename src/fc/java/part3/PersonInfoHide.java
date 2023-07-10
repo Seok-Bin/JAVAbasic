6afffc003bfad6612cc2cd59b7c459c73484d527
seokbin <dnltjrqls08@naver.com>
2023-07-10 17:31:59 +0900

    PersonVO, PersonInfoHide 파일 생성

---
+++ src/fc/java/part3/PersonInfoHide.java
@@ -0,0 +1,10 @@
+package fc.java.part3;
+
+import fc.java.model.PersonVO;
+
+public class PersonInfoHide {
+    public static void main(String[] args) {
+        PersonVO p = new PersonVO();
+        // 정보은닉으로 인하여 상태 정보에 접근이 불가
+    }
+}
