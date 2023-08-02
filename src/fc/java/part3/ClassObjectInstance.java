c6670a51755f42f4f09d3063d31f12941e28a864
seokbin <dnltjrqls08@naver.com>
2023-08-02 19:44:01 +0900

    ClassObjectInstance 파일 update

--- src/fc/java/part3/ClassObjectInstance.java
+++ src/fc/java/part3/ClassObjectInstance.java
@@ -9,9 +9,8 @@ public class ClassObjectInstance {
         StudentVO st3;  // = object
 
         st1 = new StudentVO("홍길동","컴공",20,"aaa@naver.com",20170302,"010-0000-0000");  // = instance
-        st2 = new StudentVO("나길동","컴공",20,"aaa@naver.com",20170302,"010-0000-0000");  // = instance
-        st3 = new StudentVO("김길동","컴공",20,"aaa@naver.com",20170302,"010-0000-0000");  // = instance
-
+        st2 = new StudentVO("나길동","축구",22,"aaa@naver.com",20190302,"010-0000-0000");  // = instance
+        st3 = new StudentVO("김길동","전기",23,"aaa@naver.com",20180302,"010-0000-0000");  // = instance
 
         System.out.println(st1.toString());
         System.out.println(st2.toString());
