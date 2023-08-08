6d5fa3d58f907d9326d30233c1a6e594ad199683
seokbin <dnltjrqls08@naver.com>
2023-08-08 21:47:23 +0900

    EmployeeInitTest 파일 생성

---
+++ src/fc/java/part4/EmployeeInitTest.java
@@ -0,0 +1,8 @@
+package fc.java.part4;
+
+public class EmployeeInitTest {
+    public static void main(String[] args) {
+        RempVO vo = new RempVO("홍길동", 33, "010-0000-0000", "2022-10-10", "홍보부", true);
+        System.out.println(vo);
+    }
+}
