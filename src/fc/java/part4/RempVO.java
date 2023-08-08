1441b2a727269cba88349a76ec2d7260662c081e
seokbin <dnltjrqls08@naver.com>
2023-08-08 21:44:52 +0900

    RempVO 생성자 업데이트(초기화 한 데이터를 Employee에게 전송)

--- src/fc/java/part4/RempVO.java
+++ src/fc/java/part4/RempVO.java
@@ -9,12 +9,7 @@ public class RempVO extends Employee {
     }
 
     public RempVO(String name, int age, String phone, String empDate, String dept, boolean marriage){
-        // 초기화(자식이 부모의 기억공간에 접근하여 초기화)
-        this.name = name;
-        this.age = age;
-        this.phone = phone;
-        this.empDate = empDate;
-        this.dept = dept;
-        this.marriage = marriage;
+        // 부모에게 데이터 전송
+        super(name, age, phone, empDate,dept, marriage); //  부모의 생성자를 호출
     }
 }
