696f1d3ea7dccee1927f6e86a5c866de091bbd40
seokbin <dnltjrqls08@naver.com>
2023-08-08 21:28:56 +0900

    RempVO 생성자 업데이트(초기화)

--- src/fc/java/part4/RempVO.java
+++ src/fc/java/part4/RempVO.java
@@ -7,4 +7,14 @@ public class RempVO extends Employee {
     public RempVO(){
         super(); // ==> new Employee()
     }
+
+    public RempVO(String name, int age, String phone, String empDate, String dept, boolean marriage){
+        // 초기화(자식이 부모의 기억공간에 접근하여 초기화)
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.empDate = empDate;
+        this.dept = dept;
+        this.marriage = marriage;
+    }
 }
